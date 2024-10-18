269fcb47ead841407b57323fea0fcc3defa9567f
liwook <114210093+liwook@users.noreply.github.com>
2024-10-18 15:19:13 +0800

    Add files via upload

--- code/dianpingV0.1/internal/router/router.go
+++ code/dianpingV0.1/internal/router/router.go
@@ -6,6 +6,11 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type User struct {
+	Name string `json:"name" binding:"required"`
+	Age  int    `json:"age" `
+}
+
 func NewRouter() *gin.Engine {
 	r := gin.Default()
 
@@ -13,12 +18,26 @@ func NewRouter() *gin.Engine {
 		code.WriteResponse(c, code.ErrSuccess, "pong")
 	})
 
-	r.GET("/test", func(c *gin.Context) {
-		code.WriteResponse(c, code.ErrDatabase, "not find this data")
+	r.POST("/shouldbindjson", func(ctx *gin.Context) {
+		var u User
+		err := ctx.ShouldBindJSON(&u)
+		if err != nil {
+			//返回了500
+			code.WriteResponse(ctx, code.ErrDatabase, err.Error())
+			return
+		}
+		code.WriteResponse(ctx, code.ErrSuccess, gin.H{"name": u.Name, "age": u.Age})
+	})
+	r.POST("/bindjson", func(ctx *gin.Context) {
+		var u User
+		err := ctx.BindJSON(&u)
+		if err != nil {
+			//但却返回了400
+			code.WriteResponse(ctx, code.ErrDatabase, "bindjson error") //ErrDatabase是返回500的
+			return
+		}
+		code.WriteResponse(ctx, code.ErrSuccess, gin.H{"name": u.Name, "age": u.Age})
 	})
 
-	r.GET("/test2", func(c *gin.Context) {
-		code.WriteResponse(c, code.ErrDatabase, nil)
-	})
 	return r
 }
